d49edec562815d9224ae35ea5a6c3a671a663b0b
LeQuangLam <150958041+Lamlam26072004@users.noreply.github.com>
2025-03-20 10:11:06 +0700

    cart

---
+++ be/src/controllers/cart.js
@@ -0,0 +1,33 @@
+const Cart = require("../models/cart");
+const Product = require("../models/product");
+const {Object} = require('mongodb');
+const getCartByUserId = (rep , res) =>{
+
+}
+
+const addItemToCart = (req, res ) => {
+
+}
+
+const deleteItemFromCart = (req, res ) => {
+
+ }
+
+ const increaseProductQuantity = (req , res) => {
+
+ }
+ const decreaseProductQuantity = (req , res) => {
+
+ }
+ const updateProductQuantity = (req , res) => {
+
+ }
+ module.exports = {
+    getCartByUserId,
+    addItemToCart,
+    deleteItemFromCart,
+    increaseProductQuantity,
+    decreaseProductQuantity,
+    updateProductQuantity,
+  };
+
