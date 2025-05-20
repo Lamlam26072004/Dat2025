2f8d9902b6e400b95b1dbda12e04d80b3266619f
Tiennmph33441n <Tiennmph33441@fpt.edu.vn>
2025-05-20 22:19:36 +0700

    cmcm

---
+++ fe/vite-project/src/routes/_Layout.tsx
@@ -0,0 +1,15 @@
+// import ChatBot from '@/components/ChatBot';
+import Footer from '../components/footer';
+import Header from '../components/header';
+import { createFileRoute, Outlet } from '@tanstack/react-router';
+export const Route = createFileRoute('/_layout')({
+  component: () => (
+    <div>
+      <div>
+        <Header />
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  ),
+});
